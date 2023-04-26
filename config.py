0af3a8931f014111ee8c49aae1b7e5d897321a7c
thiagomedeiros11 <thiago.wav1@gmail.com>
2023-04-26 18:53:54 -0300

    update

--- config.py
+++ config.py
@@ -1,25 +1,23 @@
 import pathlib
+import pyautogui
 
-
-window = 3423, 0, 177, 1080
-
-currentPath = pathlib.Path(__file__).parent.resolve()
-
-monstersPath = f'{currentPath}/src/images/monsters/'
-
-#TODO move to configs
 # Paths
+currentPath = pathlib.Path(__file__).parent.resolve()
 barsPath = f'{currentPath}/src/images/bars/'
 containersPath = f'{currentPath}/src/images/containers/'
 lootsPath = f'{currentPath}/src/images/loots/'
+monstersPath = f'{currentPath}/src/images/monsters/'
 
-#TODO move to configs and 
-# Coordinates
-#left, top, width, height
-gameWindow = 1680, 0, 1920, 1080
-# TODO Battle window need to be resized to minimum size
+# Windows
+window = 3423, 0, 177, 1080
 battleWindow = 3425, 372, 175, 172
 barsWindow = 3430, 145, 150, 35
-backpackWindow = 3425, 542, 175, 215
+miniMap = 3431, 31, 114, 112
 
+# Cave waypoints
 targetName = 'swamptroll' # swamp troll venore cave to south
+
+
+# Tests
+# window = pyautogui.screenshot(region=window)
+# window.save('teste1.png')
